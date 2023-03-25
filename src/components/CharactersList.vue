2ee68d2182808aa9d2c9f6e7f7597daa2617e9b9
EllenBecker <ellenbeckerdc@gmail.com>
2023-03-25 15:10:57 -0300

    Listagem de personagens

---
+++ src/components/CharactersList.vue
@@ -0,0 +1,34 @@
+<template>
+    <Button label="Listar Personagens" @click="listarPersonagens"/>
+        <DataTable :value="listaPersonagens" v-if="this.listaPersonagens.length" tableStyle="min-width: 50rem">
+            <Column field="name" header="Nome"></Column>
+            <Column field="birth_year" header="Ano de Aniversario"></Column>
+            <Column field="gender" header="Genero"></Column>
+            <Column field="eye_color" header="Cor dos olhos"></Column>
+        </DataTable>
+</template>
+  
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'CharactersList',
+    data() {
+        return {
+            listaPersonagens: []
+        }
+    },
+    methods: {
+        async listarPersonagens() {
+            const { data } = await axios.get("http://localhost:3000/star_wars", {});
+            this.listaPersonagens = data;
+            return this.listaPersonagens;
+        }
+    }
+};
+</script>
+  
+<style scoped>
+
+</style>
+  
--- src/main.js
+++ src/main.js
@@ -8,10 +8,15 @@ import 'primevue/resources/themes/lara-light-indigo/theme.css';
 import PrimeVue from 'primevue/config';
 import Button from 'primevue/button';
 import Dialog from 'primevue/dialog';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
 
 const app = createApp(App);
 app.use(PrimeVue);
 /* eslint-disable */
 app.component('Button', Button);
 app.component('Dialog', Dialog);
+app.component('DataTable', DataTable);
+app.component('Column', Column);
+
 app.mount('#app');
