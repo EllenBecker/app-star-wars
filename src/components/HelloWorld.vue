e6cefd73c280c62dc14d970360a07bfbba6525a1
EllenBecker <ellenbeckerdc@gmail.com>
2023-03-25 14:30:24 -0300

    Config PrimeVue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <Button label="Show" icon="pi pi-external-link" @click="this.visible = true" />
+    <Dialog v-model:visible="visible" modal header="Header" :style="{ width: '50vw' }">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+    </Dialog>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -35,6 +42,11 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      visible: false
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+import 'primevue/resources/themes/lara-light-indigo/theme.css';
+
+import PrimeVue from 'primevue/config';
+import Button from 'primevue/button';
+import Dialog from 'primevue/dialog';
+
+const app = createApp(App);
+app.use(PrimeVue);
+/* eslint-disable */
+app.component('Button', Button);
+app.component('Dialog', Dialog);
+app.mount('#app');
